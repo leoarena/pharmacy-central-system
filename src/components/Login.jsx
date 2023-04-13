60cb62c53dd3d133e514180cacfd0835fe0ce11b
Leonardo Arena <leonardoarena7@gmail.com>
2023-04-13 16:28:09 -0300

    html e css componente Login.jsx

---
+++ src/components/Login.jsx
@@ -0,0 +1,52 @@
+import { SCLogin } from "./styledComponents";
+import { FaClinicMedical } from "react-icons/fa";
+
+export default function Login() {
+  return (
+    <SCLogin>
+      <nav>
+        <div className="nav-left">
+          <div className="icon">
+            <FaClinicMedical />
+          </div>
+          <p>Pharmacy Central System</p>
+        </div>
+        <div className="nav-right">
+          <button className="nav-login">Login</button>
+        </div>
+      </nav>
+      <div className="container">
+        <h1>Pharmacy Central System</h1>
+        <form className="login">
+          <div className="campo"></div>
+
+          <div className="campo">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Insira seu email"
+              required
+            />
+          </div>
+
+          <div className="campo">
+            <label htmlFor="senha">Senha:</label>
+            <input
+              type="password"
+              name="senha"
+              placeholder="Insira sua senha"
+              required
+            />
+          </div>
+
+          <div className="botoes">
+            <button>Entrar</button>
+            <button>Cadastrar-se</button>
+          </div>
+          <p className="esqueceu">Esqueceu sua senha?</p>
+        </form>
+      </div>
+    </SCLogin>
+  );
+}
---
+++ src/components/styledComponents.js
@@ -0,0 +1,130 @@
+import styled from "styled-components";
+
+export const SCLogin = styled.div`
+  /* background-color: white; */
+  /* height: 50vh; */
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* border-radius: 20px; */
+  /* margin: 25% 0 0 0; */
+
+  nav {
+    background-color: skyblue;
+    width: 100vw;
+    /* height: 5%; */
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 0.5%;
+    font-size: 1.2rem;
+  }
+
+  .nav-left {
+    /* background-color: brown; */
+    display: flex;
+    align-items: center;
+    width: 50%;
+  }
+
+  nav .icon {
+    /* background-color: black; */
+    color: black;
+    padding: 0.2% 0.2% 0 0.21%;
+    margin: 0.2% 0;
+    /* border-radius: 5px; */
+    font-size: 2rem;
+  }
+
+  nav p {
+    margin: 0 1%;
+    font-weight: 600;
+  }
+
+  .nav-login {
+    font-size: 1.2rem;
+    border-radius: 5px;
+    border-style: none;
+    padding: 5px;
+  }
+
+  .nav-login:hover {
+    background-color: black;
+    color: white;
+  }
+
+  .container{
+    background-color: skyblue;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2% 3% 1.4%;
+    margin: 9% 0 0;
+    border-radius: 20px;
+    border: 1px solid black;
+  }
+  
+  h1 {
+    font-size: 2.3rem;
+    margin: 0 0 4%;
+  }
+  
+  .login {
+    /* background-color: green; */
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+  }
+
+  .campo {
+    display: flex;
+    flex-direction: column;
+    font-size: 1.2rem;
+  }
+
+  .campo label {
+    padding-left: 1%;
+    font-size: 1.3rem;
+  }
+
+  .campo input {
+    margin: 0 0 2% 0;
+    padding-left: 1%;
+    font-size: 1.3rem;
+    border-radius: 3px;
+    border-style: none;
+  }
+
+  .botoes {
+    /* background-color: blueviolet; */
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    margin: 8% 0 0;
+  }
+
+  .botoes button {
+    font-size: 1.2rem;
+    border-radius: 5px;
+    border-style: none;
+    /* padding: 2%; */
+    padding: 5px;
+  }
+
+  .botoes button:hover {
+    background-color: black;
+    color: white;
+  }
+
+  .esqueceu {
+    font-size: 1.1rem;
+    margin: 5% auto 0;
+  }
+
+  .esqueceu:hover {
+    color: blue;
+    text-decoration: underline;
+  }
+`;
