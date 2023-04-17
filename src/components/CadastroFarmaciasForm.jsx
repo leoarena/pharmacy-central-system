db3f31e6da2a7acf5eab4fb5a2743b711771baa1
Leonardo Arena <leonardoarena7@gmail.com>
2023-04-17 16:24:55 -0300

    feat: implementação usestate e useeffect no componente CadastroFarmaciasForm

--- src/components/CadastroFarmaciasForm.jsx
+++ src/components/CadastroFarmaciasForm.jsx
@@ -1,73 +1,218 @@
+import { useEffect, useState } from "react";
 import { SCCadastroFarmaciasForm } from "./styledComponents";
 
 export default function CadastroFarmaciasForm() {
+  const [inputRazaoSocial, setInputRazaoSocial] = useState("");
+  const [inputCNPJ, setInputCNPJ] = useState("");
+  const [inputNomeFantasia, setInputNomeFantasia] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputTelefone, setInputTelefone] = useState("");
+  const [inputCelular, setInputCelular] = useState("");
+  const [inputCEP, setInputCEP] = useState("");
+  const [inputNumero, setInputNumero] = useState("");
+  const [inputComplemento, setInputComplemento] = useState("");
+
+  const [inputLogradouro, setInputLogradouro] = useState("");
+  const [inputBairro, setInputBairro] = useState("");
+  const [inputCidade, setInputCidade] = useState("");
+  const [inputEstado, setInputEstado] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    if (inputCEP.length === 8) buscarCEP();
+  }, [inputCEP]);
+
+  const buscarCEP = () => {
+    console.log("deu");
+    fetch(`https://viacep.com.br/ws/${inputCEP}/json/`)
+      .then((response) => response.json())
+      .then((JSONresponse) => inputsEndereco(JSONresponse))
+      .catch((error) => console.log(error));
+  };
+
+  const inputsEndereco = (JSONresponse) => {
+    setInputLogradouro(JSONresponse.logradouro);
+    setInputBairro(JSONresponse.bairro);
+    setInputCidade(JSONresponse.localidade);
+    setInputEstado(JSONresponse.uf);
+  };
+
+  const limparInputs = () => {
+    setInputRazaoSocial("");
+    setInputCNPJ("");
+    setInputNomeFantasia("");
+    setInputEmail("");
+    setInputTelefone("");
+    setInputCelular("");
+    setInputCEP("");
+    setInputLogradouro("");
+    setInputNumero("");
+    setInputBairro("");
+    setInputCidade("");
+    setInputEstado("");
+    setInputComplemento("");
+  };
+
   return (
-    <SCCadastroFarmaciasForm>
+    <SCCadastroFarmaciasForm onSubmit={handleSubmit}>
       <div className="item">
-        <label htmlFor="razao-social">Razão Social</label>
-        <input type="text" name="razao-social" id="razao-social" required />
+        <label htmlFor="razao-social">Razão Social:</label>
+        <input
+          type="text"
+          name="razao-social"
+          id="razao-social"
+          required
+          value={inputRazaoSocial}
+          onChange={(e) => setInputRazaoSocial(e.target.value)}
+        />
       </div>
 
       <div className="item">
-        <label htmlFor="cnpj">CNPJ</label>
-        <input type="number" name="cnpj" id="cnpj" required />
+        <label htmlFor="cnpj">CNPJ:</label>
+        <input
+          type="number"
+          name="cnpj"
+          id="cnpj"
+          required
+          value={inputCNPJ}
+          onChange={(e) => setInputCNPJ(e.target.value)}
+        />
       </div>
 
       <div className="item">
-        <label htmlFor="nome-fantasia">Nome Fantasia</label>
-        <input type="text" name="nome-fantasia" id="nome-fantasia" required />
+        <label htmlFor="nome-fantasia">Nome Fantasia:</label>
+        <input
+          type="text"
+          name="nome-fantasia"
+          id="nome-fantasia"
+          required
+          value={inputNomeFantasia}
+          onChange={(e) => setInputNomeFantasia(e.target.value)}
+        />
       </div>
 
       <div className="item">
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" required />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          required
+          value={inputEmail}
+          onChange={(e) => setInputEmail(e.target.value)}
+        />
       </div>
 
       <div className="item">
-        <label htmlFor="telefone">Telefone</label>
-        <input type="tel" name="telefone" id="telefone" />
+        <label htmlFor="telefone">Telefone:</label>
+        <input
+          type="tel"
+          name="telefone"
+          id="telefone"
+          value={inputTelefone}
+          onChange={(e) => setInputTelefone(e.target.value)}
+        />
       </div>
 
       <div className="item">
-        <label htmlFor="celular">Celular</label>
-        <input type="tel" name="celular" id="celular" required />
+        <label htmlFor="celular">Celular:</label>
+        <input
+          type="tel"
+          name="celular"
+          id="celular"
+          required
+          value={inputCelular}
+          onChange={(e) => setInputCelular(e.target.value)}
+        />
       </div>
 
       {/* <div className="divider"></div> */}
 
       <div className="item">
-        <label htmlFor="cep">CEP</label>
-        <input type="number" name="cep" id="cep" required />
+        <label htmlFor="cep">CEP:</label>
+        <input
+          type="number"
+          name="cep"
+          id="cep"
+          required
+          maxLength={8}
+          value={inputCEP}
+          onChange={(e) => setInputCEP(e.target.value)}
+        />
       </div>
 
       <div className="item">
-        <label htmlFor="logradouro">Logradouro</label>
-        <input type="text" name="logradouro" id="logradouro" required />
+        <label htmlFor="logradouro">Logradouro:</label>
+        <input
+          type="text"
+          name="logradouro"
+          id="logradouro"
+          required
+          value={inputLogradouro}
+          onChange={(e) => setInputLogradouro(e.target.value)}
+        />
       </div>
 
       <div className="item">
-        <label htmlFor="numero">Número</label>
-        <input type="number" name="numero" id="numero" required />
+        <label htmlFor="numero">Número:</label>
+        <input
+          type="number"
+          name="numero"
+          id="numero"
+          required
+          value={inputNumero}
+          onChange={(e) => setInputNumero(e.target.value)}
+        />
       </div>
 
       <div className="item">
-        <label htmlFor="bairro">Bairro</label>
-        <input type="text" name="bairro" id="bairro" required />
+        <label htmlFor="bairro">Bairro:</label>
+        <input
+          type="text"
+          name="bairro"
+          id="bairro"
+          required
+          value={inputBairro}
+          onChange={(e) => setInputBairro(e.target.value)}
+        />
       </div>
 
       <div className="item">
-        <label htmlFor="cidade">Cidade</label>
-        <input type="text" name="cidade" id="cidade" required />
+        <label htmlFor="cidade">Cidade:</label>
+        <input
+          type="text"
+          name="cidade"
+          id="cidade"
+          required
+          value={inputCidade}
+          onChange={(e) => setInputCidade(e.target.value)}
+        />
       </div>
 
       <div className="item">
-        <label htmlFor="estado">Estado</label>
-        <input type="text" name="estado" id="estado" required />
+        <label htmlFor="estado">Estado:</label>
+        <input
+          type="text"
+          name="estado"
+          id="estado"
+          required
+          value={inputEstado}
+          onChange={(e) => setInputEstado(e.target.value)}
+        />
       </div>
 
       <div className="item">
-        <label htmlFor="complemento">Complemento</label>
-        <input type="text" name="complemento" id="complemento" />
+        <label htmlFor="complemento">Complemento:</label>
+        <input
+          type="text"
+          name="complemento"
+          id="complemento"
+          value={inputComplemento}
+          onChange={(e) => setInputComplemento(e.target.value)}
+        />
       </div>
 
       {/* <div className="item">
@@ -81,7 +226,9 @@ export default function CadastroFarmaciasForm() {
       </div> */}
 
       <div className="botoes">
-        <button type="button">Limpar</button>
+        <button type="button" onClick={limparInputs}>
+          Limpar
+        </button>
         <button type="submit">Salvar</button>
       </div>
     </SCCadastroFarmaciasForm>
