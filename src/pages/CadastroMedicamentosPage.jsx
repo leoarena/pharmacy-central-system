d54ec390c5f5d4ca920ad8d6523fbbc9d67ccfa4
Leonardo Arena <leonardoarena7@gmail.com>
2023-04-19 16:09:57 -0300

    feat: título página cadastro de medicamentos

--- src/pages/CadastroMedicamentosPage.jsx
+++ src/pages/CadastroMedicamentosPage.jsx
@@ -1,8 +1,13 @@
 import { SCCadastroMedicamentosPage } from "../components/styledComponents";
 import Nav from "../components/Nav";
 import CadastroMedicamentosForm from "../components/CadastroMedicamentosForm";
+import { useEffect } from "react";
 
 export default function CadastroMedicamentosPage() {
+  useEffect(() => {
+    document.title = "Cadastro de Medicamentos";
+  }, []);
+
   return (
     <SCCadastroMedicamentosPage>
       <Nav />
