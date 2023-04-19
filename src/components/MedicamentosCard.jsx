15318dc9c15b74f18b4ab31ded2b9e772affcf88
Leonardo Arena <leonardoarena7@gmail.com>
2023-04-19 14:31:23 -0300

    feat: implementação inicial da visualização por cards

---
+++ src/components/MedicamentosCard.jsx
@@ -0,0 +1,30 @@
+import { Card } from "react-bootstrap";
+import { FaBriefcaseMedical } from "react-icons/fa";
+
+export default function MedicamentosCard({ dadosFormulario }) {
+  return (
+    <div className="d-flex flex-wrap justify-content-between">
+      {Object.keys(dadosFormulario).map((indexMedicamento) => {
+        return (
+          <Card
+            key={indexMedicamento}
+            className="m-3"
+            style={{ width: "15vw" }}
+          >
+            <div className="d-flex justify-content-center m-1 mt-4 fs-1">
+              <FaBriefcaseMedical />
+            </div>
+            <Card.Body>
+              <Card.Title>
+                {dadosFormulario[indexMedicamento].inputMedicamento}
+              </Card.Title>
+              <Card.Text>
+                {dadosFormulario[indexMedicamento].inputLaboratorio}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        );
+      })}
+    </div>
+  );
+}
