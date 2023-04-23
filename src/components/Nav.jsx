1b52799b7d55c332f58269ea25b180adc4ba13d4
Leonardo Arena <leonardoarena7@gmail.com>
2023-04-23 02:12:33 -0300

    fix: correção na nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,54 +9,60 @@ export default function NavComponent() {
 
   return (
     <Navbar className="skyblue" expand="lg" style={{ height: "45px" }}>
-      <Nav.Link href="/farmacias" className="d-flex align-items-center">
-        <FontAwesomeIcon icon={faLaptopMedical} className="fa-2x mx-2" />
-        <span>Pharmacy Central System</span>
-      </Nav.Link>
       {rotaAtual === "/" ? (
-        <div className="ms-auto">
-          <span className="me-2">Login</span>
-        </div>
+        <>
+          <FontAwesomeIcon icon={faLaptopMedical} className="fa-2x mx-2" />
+          <span>Pharmacy Central System</span>
+          <div className="ms-auto">
+            <span className="me-2">Login</span>
+          </div>
+        </>
       ) : (
-        <Navbar.Collapse>
-          <Nav className="ms-auto">
-            {rotaAtual === "/farmacias" ||
-            rotaAtual === "/cadastro-farmacia" ? (
-              <>
-                <Button
-                  variant=""
-                  href="/farmacias"
-                  className="border-0 fw-bold"
-                >
-                  Farmácias
-                </Button>
-                <Button variant="" href="/medicamentos" className="border-0">
-                  Medicamentos
-                </Button>
-              </>
-            ) : (
-              <>
-                <Button variant="" href="/farmacias" className="border-0">
-                  Farmácias
-                </Button>
-                <Button
-                  variant=""
-                  href="/medicamentos"
-                  className="border-0 fw-bold"
-                >
-                  Medicamentos
-                </Button>
-              </>
-            )}
-            <Button
-              variant="danger"
-              onClick={() => navigate("/")}
-              className="me-1 border-dark"
-            >
-              Sair
-            </Button>
-          </Nav>
-        </Navbar.Collapse>
+        <>
+          <Nav.Link href="/farmacias" className="d-flex align-items-center">
+            <FontAwesomeIcon icon={faLaptopMedical} className="fa-2x mx-2" />
+            <span>Pharmacy Central System</span>
+          </Nav.Link>
+          <Navbar.Collapse>
+            <Nav className="ms-auto">
+              {rotaAtual === "/farmacias" ||
+              rotaAtual === "/cadastro-farmacia" ? (
+                <>
+                  <Button
+                    variant=""
+                    href="/farmacias"
+                    className="border-0 fw-bold"
+                  >
+                    Farmácias
+                  </Button>
+                  <Button variant="" href="/medicamentos" className="border-0">
+                    Medicamentos
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Button variant="" href="/farmacias" className="border-0">
+                    Farmácias
+                  </Button>
+                  <Button
+                    variant=""
+                    href="/medicamentos"
+                    className="border-0 fw-bold"
+                  >
+                    Medicamentos
+                  </Button>
+                </>
+              )}
+              <Button
+                variant="danger"
+                onClick={() => navigate("/")}
+                className="me-1 border-dark"
+              >
+                Sair
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        </>
       )}
     </Navbar>
   );
